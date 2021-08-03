fa6cc13ca47772afec3642f8a5a1606963ac8ca1
tae2089 <tae2089002@gmail.com>
2021-08-03 21:07:56 +0900

    11

---
+++ .github/actions/make-issue/src/index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+    const issueTitle = core.gitInput('issueTitle');
+    const joke = core.getInput('joke');
+    const token = core.getInput('repoToken');
+
+    try {
+        const octokit = new github.GitHub(token);
+        const newIssue = await octokit.issues.create({
+            repo: github.context.repo.repo,
+            owner: github.context.repo.owner,
+            title: issueTitle,
+            body: '![Joke Image](' + joke + ')',
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+run();
